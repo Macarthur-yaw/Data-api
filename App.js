1180912ccb8b775ea80f884270dada4bbd7ce2e8
Kevin <arthurkevin1260@gmail.com>
2023-07-07 03:22:08 +0100

    Added some functionalities

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 const express=require('express')
 const app=express()
-const book = require('./data')
+const book = require('./data');
+const books = require('./data');
 
 app.listen(5000,()=>{
     console.log("Listening to port 5000");
 // console.log(book)
 })
-app.get('/api/books',(req,res)=>{
+
+//specifying the id of the route 
+
+app.get('/api/books/:id',(req,res)=>{
+
+    const {id}=req.params
+    
+    const newBooks =book.find((product)=>
+    product.id === id)
+    console.log(req.params)
     // res.send("Created an Api for a book");
-res.status(200).send(book)
+res.status(200).send(newBooks)
+})
+app.get('*',(req,res)=>{
+
+    res.status(404).send("No Resources found")
 })
